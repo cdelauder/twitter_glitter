d745ca245527d4f1a66408d5a5410d520f79284d
& <pair+cdelauder.tommyixi@devbootcamp.com>
2014-05-15 11:14:43 -0700

    Added the login/home page and user can now log in

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,14 @@
 get '/' do
-  # Look in app/views/index.erb
-  erb :index
+  erb :home
+  # Let's display a list of the users foll
+end
+
+post '/' do
+  @user = User.where(username: params[:username], password: params[:password]).first
+  if @user
+    session[:user_id]=@user.id
+    redirect '/'
+  else
+    erb :error
+  end
 end
