1f8183e6ab6be22b5d5183abcc469861c5789266
Michael Ohm & will tsay <pair+michaelohm.willtsay@devbootcamp.com>
2014-05-15 11:23:58 -0700

    can create an account now

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -1,4 +1,15 @@
 get '/' do
   # Look in app/views/index.erb
+  @message = params[:message]
   erb :index
+end
+
+
+get '/create' do
+  erb :create
+end
+
+post '/create' do
+  @user = User.create(params[:user])
+  redirect '/?message=account%20created'
 end
