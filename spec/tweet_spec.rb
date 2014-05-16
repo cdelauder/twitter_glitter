0088032b4d66d581a2fcbfdb18ac0ac9d3839e61
Chris DeLauder <cdelauder@gmail.com>
2014-05-15 21:41:11 -0700

    created tweet model tests, includes relation test

--- spec/tweet_spec.rb
+++ spec/tweet_spec.rb
@@ -1,5 +1,16 @@
 require 'spec_helper'
 
 describe Tweet do
-  pending "add some examples to (or delete) /Users/stupo/ruby/glitter/Rakefile"
+  it "should be a string of less than 140 characters" do
+    tweet = Tweet.create(user_id: 1, content: "This is a tweet")
+
+    expect(tweet.content).to eq("This is a tweet")
+  end
+  it "should belong to a user" do
+    User.create(name: 'somejerk', username: 'asdfasdf', password: 'somejerk', location: 'sf')
+    tweet = Tweet.create(user_id: 1, content: "This is a tweet")
+    user = User.find(tweet.user_id)
+
+    expect(user.name).to eq("somejerk")
+  end
 end
